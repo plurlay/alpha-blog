faaab747e472dd801fba17468bf06b825075e503
plurlay <lurlayp@gmail.com>
2016-12-06 15:39:49 -0800

    extract redondancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
     class ArticlesController < ApplicationController
-
+         before_action :set_article, only: [:edit, :update, :show, :destroy]
 
         def index
          @articles = Article.all
@@ -10,7 +10,6 @@
           end
 
           def edit
-            @article = Article.find(params[:id])
           end
 
           def create
@@ -25,7 +24,6 @@
           end
 
           def update()
-               @article = Article.find(params[:id])
                if @article.update(article_params)
                    flash[:notice] = "Article was successfully updated"
                    redirect_to article_path(@article)
@@ -35,16 +33,19 @@
           end
 
           def show
-            @article = Article.find(params[:id])
           end
           
           def destroy
-          @article = Article.find(params[:id])
           @article.destroy
           flash[:notice] = "Article was successfully deleted"
               
           end
-
+          
+          private
+          def set_article
+            @article = Article.find(params[:id])
+          end
+        
           private
           def article_params
             params.require(:article).permit(:title, :description)

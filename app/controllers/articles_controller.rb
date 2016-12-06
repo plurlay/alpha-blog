8dde408e2eed981a6e35bcd10dad045101e34694
plurlay <lurlayp@gmail.com>
2016-12-06 14:42:51 -0800

    Updated all of the pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,26 +1,46 @@
-class ArticlesController < ApplicationController
+    class ArticlesController < ApplicationController
 
-      def new
-        @article = Article.new
-      end
 
-      def create
-        @article = Article.new(article_params)
-        if @article.save
-           flash[:notice] = "Article was successfully created"
-          redirect_to article_path(@article)
-         else
-         render 'new'
-      end
-      end
+        def index
+         @articles = Article.all
+        end
+        
+          def new
+            @article = Article.new
+          end
 
-      def show
-        @article = Article.find(params[:id])
-      end
+          def edit
+            @article = Article.find(params[:id])
+          end
 
-      private
-      def article_params
-        params.require(:article).permit(:title, :description)
-      end
+          def create
+            @article = Article.new(article_params)
+            if @article.save
+               flash[:notice] = "Article was successfully created"
+              redirect_to article_path(@article)
+             else
+             render 'new'
+          end
 
-end
+          end
+
+          def update()
+               @article = Article.find(params[:id])
+               if @article.update(article_params)
+                   flash[:notice] = "Article was successfully updated"
+                   redirect_to article_path(@article)
+                   else
+                   render 'edit'
+               end
+          end
+
+          def show
+            @article = Article.find(params[:id])
+          end
+
+          private
+          def article_params
+            params.require(:article).permit(:title, :description)
+          end
+
+    end

f737289184ee1d2d353ae59b432495915e9c1e6f
plurlay <lurlayp@gmail.com>
2016-12-16 07:59:05 -0800

    added index and updated shoe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController <ApplicationController
+
+  def index
+    @users = User.all
+  end
+  
   def new
     @user = User.new
   end
-  
+
     def create
     @user = User.new(user_params)
         if @user.save
@@ -15,8 +20,8 @@ class UsersController <ApplicationController
     def edit
       @user = User.find(params[:id])
     end
-    
-    def update 
+
+    def update
       @user = User.find(params[:id])
         if @user.update(user_params)
         flash[:success] = "Your account was updated successfully"
@@ -25,16 +30,16 @@ class UsersController <ApplicationController
             render 'edit'
         end
     end
-    
+
     def show
        @user = User.find(params[:id])
     end
-    
-        
+
+
     private
     def user_params
-       params.require(:user).permit(:username, :email, :password) 
+       params.require(:user).permit(:username, :email, :password)
     end
-    
-    
+
+
 end

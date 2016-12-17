af17bb75231a1016749573f160b6caed5494909d
plurlay <lurlayp@gmail.com>
2016-12-16 21:28:42 -0800

    complete authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,6 @@ Rails.application.routes.draw do
   resources :users, except: [:new]
 
   get 'login', to: 'sessions#new'
-  post 'login', to: 'session#create'
-  delete 'logout' to: 'session#destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
